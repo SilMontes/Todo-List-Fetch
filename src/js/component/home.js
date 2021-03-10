ec06e96b58ea1d0fc6e7a96bdbefa407cb4df49c
SilMontes <Silmontes2001@gmail.com>
2021-03-10 19:02:55 +0000

    Avance Get and Put

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,77 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import React, { useState, useEffect } from "react";
+import { ListaTareas } from "./listatareas";
+import swal from "sweetalert";
 //create your first component
+
 export function Home() {
+	const [list, setList] = useState([]);
+	const [task, setTask] = useState("");
+
+	// fetch("https://assets.breatheco.de/apis/fake/todos/user/SilMontes", {
+	// 	method: "PUT",
+	// 	headers: {
+	// 		"Content-Type": "application/json"
+	// 	},
+	// 	body: JSON.stringify(list)
+	// })
+	// 	.then(resp => resp.json())
+	// 	.then(data => console.log(data));
+
+	useEffect(() => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/SilMontes")
+			.then(resp => resp.json())
+			.then(data => setList(data));
+	}, []);
+	const myAlert = () => {
+		swal({
+			title: "Oops!",
+			text: "Need to add a task",
+			icon: "warning",
+			button: "Okay!"
+		});
+	};
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		if (task === "") {
+			myAlert();
+		} else {
+			setList([...list, { label: task, done: false }]);
+		}
+		setTask("");
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/SilMontes", {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(list)
+		})
+			.then(resp => resp.json())
+			.then(data => console.log(data));
+	};
+	const handleChange = e => {
+		setTask(e.target.value);
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<React.Fragment>
+			<div className="text-center">
+				<p>todos</p>
+			</div>
+			{/*<p>{JSON.stringify(task)}</p>
+			<p>{JSON.stringify(list)}</p>*/}
+			<div className="formDiv">
+				<form className="formulario" onSubmit={e => handleSubmit(e)}>
+					<input
+						type="text"
+						placeholder="What needs to be done?"
+						onChange={e => handleChange(e)}
+						value={task}
+					/>
+				</form>
+				<ListaTareas list={list} updateList={setList} />
+				<cite>{"Pending tasks: " + list.length}</cite>
+			</div>
+		</React.Fragment>
 	);
 }
---
+++ src/js/component/listatareas.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "../../styles/index.scss";
+import PropTypes from "prop-types";
+
+export function ListaTareas(props) {
+	const deleteTask = index => {
+		const newList = props.list.filter((item, indexit) => {
+			return indexit != index;
+		});
+
+		props.updateList(newList);
+	};
+	return (
+		<div className="listaDiv">
+			<ul>
+				{props.list.map((tarea, index) => {
+					return (
+						<div className="lista" key={index}>
+							<li>
+								<i
+									id="pushpin"
+									className="fas fa-thumbtack"></i>
+								{tarea.label}
+								<button onClick={() => deleteTask(index)}>
+									<i className="fas fa-times"></i>
+								</button>
+							</li>
+						</div>
+					);
+				})}
+			</ul>
+		</div>
+	);
+}
+ListaTareas.propTypes = {
+	list: PropTypes.array,
+	updateList: PropTypes.func
+};
